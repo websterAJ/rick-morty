6ee3e1571125c62a28b831f023d482d6a97a1eb1
Alexander Torres <alexander20012@hotmail.com>
2023-05-08 04:20:53 -0400

    ultimas modificaciones

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,14 @@
-export default function SearchBar(props) {
+import { useState } from "react";
+
+const SearchBar =  ({onSearch, agregar}) => {
+   const [id, setId] = useState('');
+   let handleChange = (event) => {setId(event.target.value)}
    return (
-      <div class="flex justify-center items-center">
-        <div class="relative"> 
-            <div class="absolute top-4 left-3">
-                 <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
-            </div>
-            <input type='search' class="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none" placeholder=""/>
-            <div class="absolute top-2 right-2">
-                <button class="h-10 w-20 text-white rounded-lg bg-red-500 hover:bg-red-600" onClick={props.onSearch}>Agregar</button>
-            </div>
-        </div>
+      <div>
+         <input type='search' onChange = {handleChange} value = {id} />
+         <button onClick = { () => {onSearch(id);  setId("") } } >Agregar</button> 
       </div>
-      
    );
 }
+
+export default SearchBar;
